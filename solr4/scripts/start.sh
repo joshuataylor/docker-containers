54487646e6d594750bb7f5b5a6b3f0adcd5e4324
Nick Schuch <nick@myschuch.com>
2014-03-14 11:40:20 +1000

    Adds a solr 4.x container.

---
+++ solr4/scripts/start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##
+# SSHD.
+##
+
+# Global ssh config.
+if [ -f '/etc/conf/sshd/sshd_config' ]; then
+  scp /etc/conf/sshd/sshd_config /etc/ssh/sshd_config
+fi
+
+# Root user.
+if [ -f '/etc/conf/sshd/root_authorized_keys' ]; then
+  mkdir -p /root/.ssh
+  scp /etc/conf/sshd/root_authorized_keys /root/.ssh/authorized_keys
+  chmod 400 /root/.ssh/authorized_keys
+  chown root:root -R /root/.ssh
+fi
+
+##
+# Rsyslog.
+##
+
+if [ -f '/etc/conf/rsyslog/rsyslog.conf' ]; then
+  scp /etc/conf/rsyslog/rsyslog.conf /etc/rsyslog.conf
+fi
+
+##
+# Supervisord.
+##
+
+supervisord -n -c /etc/supervisord.conf
