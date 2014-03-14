1e47c959d7cfbf3886abcb4c0090429294f19f43
vagrant <docker.dev>
2014-03-14 18:33:01 +1100

    Adds PHP 5.4 container.

---
+++ lamp54/scripts/start.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+##
+# Copy files into specific directories.
+##
+
+##
+# Apache.
+##
+
+if [ -f '/etc/conf/apache/vhost.conf' ]; then
+  rm -f /etc/apache2/sites-enabled/*
+  scp /etc/conf/apache/vhost.conf /etc/apache2/sites-available/drupal
+  ln -s /etc/apache2/sites-available/drupal /etc/apache2/sites-enabled/drupal
+fi
+
+##
+# PHP.
+##
+
+if [ -f '/etc/conf/php/php.ini' ]; then
+  scp /etc/conf/php/php.ini /etc/php5/apache2/php.ini
+  scp /etc/conf/php/php.ini /etc/php5/cli/php.ini
+fi
+
+##
+# Mysql.
+##
+
+if [ -f '/etc/conf/mysql/my.cnf' ]; then
+  scp /etc/conf/mysql/my.cnf /etc/mysql/my.cnf
+fi
+chown -R mysql:mysql /var/lib/mysql
+
+##
+# Cron.
+##
+
+if [ -d '/etc/conf/cron' ]; then
+  scp /etc/conf/cron/* /etc/cron.d/
+fi
+
+##
+# SSHD.
+##
+
+# Global ssh config.
+if [ -f '/etc/conf/sshd/sshd_config' ]; then
+  scp /etc/conf/sshd/sshd_config /etc/ssh/sshd_config
+fi
+
+# Root user.
+if [ -f '/etc/conf/sshd/root_authorized_keys' ]; then
+  mkdir -p /root/.ssh
+  scp /etc/conf/sshd/root_authorized_keys /root/.ssh/authorized_keys
+  chmod 400 /root/.ssh/authorized_keys
+  chown root:root -R /root/.ssh
+fi
+
+# Deployer user.
+if [ -f '/etc/conf/sshd/deployer_authorized_keys' ]; then
+  mkdir -p /home/deployer/.ssh
+  scp /etc/conf/sshd/deployer_authorized_keys /home/deployer/.ssh/authorized_keys
+  chmod 400 /home/deployer/.ssh/authorized_keys
+  chown deployer:deployer -R /home/deployer/.ssh
+fi
+
+##
+# Permissions.
+##
+
+chown -R deployer:www-data /var/www
+
+##
+# Rsyslog.
+##
+
+if [ -f '/etc/conf/rsyslog/rsyslog.conf' ]; then
+  scp /etc/conf/rsyslog/rsyslog.conf /etc/rsyslog.conf
+fi
+
+##
+# Supervisord.
+##
+
+supervisord -n -c /etc/supervisord.conf
