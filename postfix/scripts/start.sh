ed5046867941ee53577b945acf3e58befde5a5a5
Peter Lieverdink <me@cafuego.net>
2014-04-08 14:08:19 +1000

    Add supervisord config for postfix. Use standard startup script.

--- postfix/scripts/start.sh
+++ postfix/scripts/start.sh
@@ -1,9 +1,36 @@
-#!/bin/sh
-# init-like script for the postfix container
+#!/bin/bash
 
-# Run tasks as jobs where possible, so we can wait on them.
-/usr/sbin/rsyslogd -n &
-/usr/sbin/sshd -D &
-/usr/sbin/postfix start # postfix won't get waited on.
+##
+# Copy files into specific directories.
+##
 
-wait
+##
+# SSHD.
+##
+
+# Global ssh config.
+if [ -d '/etc/conf/sshd' ]; then
+  rsync -avz /etc/conf/sshd/* /etc/ssh/
+fi
+
+# Root user.
+if [ -f '/etc/conf/sshd/root_authorized_keys' ]; then
+  mkdir -p /root/.ssh
+  scp /etc/conf/sshd/root_authorized_keys /root/.ssh/authorized_keys
+  chmod 400 /root/.ssh/authorized_keys
+  chown root:root -R /root/.ssh
+fi
+
+##
+# Rsyslog.
+##
+
+if [ -f '/etc/conf/rsyslog/rsyslog.conf' ]; then
+  scp /etc/conf/rsyslog/rsyslog.conf /etc/rsyslog.conf
+fi
+
+##
+# Supervisord.
+##
+
+supervisord -n -c /etc/supervisord.conf
