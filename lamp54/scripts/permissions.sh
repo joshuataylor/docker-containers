d1a3b267ce70a45bc412109a8fb5572f4bbd6dba
Nick Schuch <nick@myschuch.com>
2014-05-02 09:59:30 +1000

    Adds permissions script to lamp54 container.

---
+++ lamp54/scripts/permissions.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# Name: permissions.sh
+# Author: Nick Schuch (nick@pnx.com.au)
+# Comment: This is a script to enforce permissions best practices.
+
+BASE='/var/www'
+USER='deployer'
+GROUP='www-data'
+
+setPermissions() {
+  # Set all the permissions!
+  chown -R ${USER}:${GROUP} ${BASE}/shared/files
+  find ${BASE}/shared/files -type d -exec chmod 775 {} \;
+  find ${BASE}/shared/files -type f -exec chmod 664 {} \;
+
+  # Make sure we have a paper trail.
+  logger Permissions have been updated.
+}
+
+while true; do
+  setPermissions
+  sleep 15m
+done
