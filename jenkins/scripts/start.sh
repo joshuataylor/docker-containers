4b4f189cbd5974ebeacd8d86e5acc07d076a3a82
Nick Schuch <nick@myschuch.com>
2014-06-26 12:17:55 +1000

    Adds flexibility to jenkins (ssh and config).

---
+++ jenkins/scripts/start.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+##
+# Copy files into specific directories.
+##
+
+##
+# Cron.
+##
+
+if [ -d '/etc/conf/cron' ]; then
+  scp /etc/conf/cron/* /etc/cron.d/
+fi
+
+##
+# SSHD.
+##
+
+# Global ssh config.
+if [ -d '/etc/conf/sshd' ]; then
+  rsync -avz /etc/conf/sshd/* /etc/ssh/
+fi
+
+# Root user.
+if [ -f '/etc/conf/sshd/root_authorized_keys' ]; then
+  mkdir -p /root/.ssh
+  scp /etc/conf/sshd/root_authorized_keys /root/.ssh/authorized_keys
+  chmod 400 /root/.ssh/authorized_keys
+  chown root:root -R /root/.ssh
+fi
+
+##
+# Rsyslog.
+##
+
+if [ -f '/etc/conf/rsyslog/rsyslog.conf' ]; then
+  scp /etc/conf/rsyslog/rsyslog.conf /etc/rsyslog.conf
+fi
+
+##
+# Jenkins.
+##
+
+if [ -d '/etc/conf/jenkins' ]; then
+  rsync -avz /etc/conf/jenkins/* /var/lib/jenkins
+fi
+chown -R jenkins:jenkins /var/lib/jenkins
+
+##
+# Supervisord.
+##
+
+supervisord -n -c /etc/supervisord.conf
