fb8a2d37c0378e256ae78ae50ed6573a5a8fa6fd
Nick Schuch <nick@myschuch.com>
2014-08-01 20:02:45 +1000

    Fixes issue with bind start.sh

--- bind/scripts/start.sh
+++ bind/scripts/start.sh
@@ -5,11 +5,13 @@
 ##
 
 # We swap out the DEV IP ADRESS so we can point *.dev to any IP we want.
+DEV_DB="/etc/bind/db.dev"
 if [ ! -z "${DEV_IP_ADDRESS}" ]; then
-  sed "s/DEV_IP_ADDRESS/${DEV_IP_ADDRESS}/" /etc/bind/db.dev
+  DEV=$(sed "s/DEV_IP_ADDRESS/${DEV_IP_ADDRESS}/" ${DEV_DB})
 else
-  sed "s/DEV_IP_ADDRESS/127.0.0.1/" /etc/bind/db.dev
+  DEV=$(sed "s/DEV_IP_ADDRESS/127.0.0.1/" ${DEV_DB})
 fi
+echo $DEV > /etc/bind/db.dev
 
 ##
 # Supervisord.
